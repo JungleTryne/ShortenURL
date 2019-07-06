50a0a7de645d989a9152f614e5aa2dcc5367b9b8
Danila Mishin <danila.mishin.2001@gmail.com>
2019-07-06 23:27:27 +0300

    connected with DB

--- shortenurl/main/models.py
+++ shortenurl/main/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class URLPair(models.Model):
+    initial_url = models.CharField(max_length=512)
+    new_url = models.CharField(max_length=128)
--- shortenurl/mshin_shortener/shorten.py
+++ shortenurl/mshin_shortener/shorten.py
@@ -1,5 +1,6 @@
 import time
 import random
+from main.models import URLPair
 
 def generate_code(length: int) -> str:
     alpha = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
@@ -13,5 +14,12 @@ def generate_code(length: int) -> str:
 
 def shorten_url(url) -> str:
     base_url = "127.0.0.1:8000/"
-    code = generate_code(6)
+    all_urls = URLPair.objects.all().filter(initial_url=url)
+    if len(all_urls) == 0:
+        code = generate_code(6)
+        pair = URLPair(initial_url=url, new_url=code)
+        pair.save()
+    else:
+        existing_code = list(all_urls)[0]
+        code = existing_code.new_url
     return base_url+code
